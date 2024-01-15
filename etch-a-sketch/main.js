ddc421ef5be7d25731d5a13ae262b1b8da492338
abu <abukim1@gmail.com>
2024-01-15 12:02:35 +0900

    Changed to flex and added button for changing grid size below 100

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,25 +1,45 @@
-
-const resetButton = document.querySelector('button');
-
 /* Selects the CLASS type of container hence the (.)container */
 const container = document.querySelector('.container');
-const sizeOfGrid = 16;
+const resetButton = document.querySelector('button');
+const sizeOfGrid = 16
 
 const createGrid = (sizeOfGrid) => {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+
   for (let i = 0; i < sizeOfGrid; i++){
     const row = document.createElement('div');
     row.classList.add('grid-row');
 
     for (let j = 0; j < sizeOfGrid; j++){
+      const widthAndHeight = 960/sizeOfGrid;
       const gridBox = document.createElement('div');
       gridBox.classList.add('grid-box');
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
       gridBox.addEventListener("mouseover", (event) => {
         gridBox.style.backgroundColor = '#3a647d';
       });
       row.appendChild(gridBox);
     }
-    container.appendChild(row);
+    wrapper.appendChild(row);
   }
+  container.appendChild(wrapper);
 }
 
-createGrid(sizeOfGrid);
+
+resetButton.addEventListener('click', () => {
+  let userCreateGridSize = Number(prompt('Please enter the size of grid you want (Maximum grid size is 100'));
+
+  while(userCreateGridSize > 101){
+  userCreateGridSize = Number(prompt('Please enter the size of grid you want (Maximum grid size is 100)'));
+
+  }
+  const wrapper = document.querySelector('.wrapper');
+
+  if (!wrapper){
+    createGrid(userCreateGridSize)
+  } else {
+    wrapper.remove();
+  }
+});
